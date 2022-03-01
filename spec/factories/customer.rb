95a11e4424e9d38f049c120a16db31b4a1cfe596
Allysonfreitas210695 <allysonfr83@gmail.com>
2022-03-01 13:41:56 -0300

    conhecendo o factoryGirl

---
+++ spec/factories/customer.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :customer do
+    name { "Allyson Bruno" }
+    email { "allysion2320@gmail.com" }
+  end
+end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "Create a Custumer" do
+    customer = create(:customer)
+    expect(customer.full_name).to eq("Sr. Allyson Bruno")
+  end
 end
