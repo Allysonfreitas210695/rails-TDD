3513e486c77bfe9913f564820d2c4c582751befe
Allysonfreitas210695 <allysonfr83@gmail.com>
2022-03-01 15:16:00 -0300

    Associaçoes(belongs_to)

---
+++ spec/models/order_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Order, type: :model do
+  it "Tem 1 pedido" do
+    order = create(:order)
+    puts order.description
+    puts order.customer
+    puts order.customer.name
+    # be_kind_of ou be_an ou be_a
+    expect(order.customer).to be_an(Customer)
+  end
+
+  it "Tem 1 pedido" do
+    customer = create(:customer)
+    order = create(:order, customer: customer)
+    # puts order.description
+    # puts order.customer
+    # puts order.customer.name
+    # be_kind_of ou be_an ou be_a
+    expect(order.customer).to be_an(Customer)
+  end
+end
