7442248353f200fbd81ce11a1606a65395a950e9
Allysonfreitas210695 <allysonfr83@gmail.com>
2022-03-01 13:49:54 -0300

    gem faker

--- spec/factories/customer.rb
+++ spec/factories/customer.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :customer do
-    name { "Allyson Bruno" }
-    email { "allysion2320@gmail.com" }
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
   end
 end
