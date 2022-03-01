2841f2fe29a3284b93734ffaba942e1bfa835ee2
Allysonfreitas210695 <allysonfr83@gmail.com>
2022-03-01 14:07:28 -0300

    herança

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -6,6 +6,16 @@ RSpec.describe Customer, type: :model do
     expect(customer.full_name).to eq("Sr. allyson")
   end
 
+  it "Herança" do
+    customer = create(:customer_vip)
+    expect(customer.vip).to be true
+  end
+
+  it "Herança" do
+    customer = create(:customer_default)
+    expect(customer.name).not_to be_nil
+  end
+
   it "#full_name" do
     customer = create(:customer)
     expect(customer.full_name).to start_with("Sr.")
