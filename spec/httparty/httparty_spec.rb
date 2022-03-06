1df738b95e4b8a42a281ba92e958125a58fdc3a9
Allysonfreitas210695 <allysonfr83@gmail.com>
2022-03-05 21:33:04 -0300

    VCR

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -1,13 +1,15 @@
 describe "HTTParty" do
   it "content-type" do
-    stub_request(:get, "https://jsonplaceholder.typicode.com/users/1")
-    .to_return(status: 200, body: "", headers: {'content-type': 'application/json'})
+    # stub_request(:get, "https://jsonplaceholder.typicode.com/users/1")
+    # .to_return(status: 200, body: "", headers: { 'content-type': 'application/json' })
 
-    response = HTTParty.get('https://jsonplaceholder.typicode.com/users/1')
-    content_type = response.headers['content-type']
-    code = response.code
+    VCR.use_cassette("jsonplaceholder/posts/2") do
+      response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/1')
+      content_type = response.headers['content-type']
+      code = response.code
+      expect(content_type).to match(/application\/json/)
+      expect(code).to eql(200)
+    end
 
-    expect(content_type).to match(/application\/json/)
-    expect(code).to eql(200)
   end
 end
