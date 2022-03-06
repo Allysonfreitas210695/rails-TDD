4c7066bc8120342f131d5cb6ad8d25d35ab22842
Allysonfreitas210695 <allysonfr83@gmail.com>
2022-03-06 17:32:01 -0300

    testando Views

---
+++ spec/features/customers_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.feature "Customers", type: :feature do
+
+  context "navigating" do
+    it "#index" do
+      visit(customers_path)
+      page.html
+      expect(page).to  have_current_path(customers_path)
+    end
+  end
+
+end
